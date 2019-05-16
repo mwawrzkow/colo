162e794d681b16542ec50ee95465f7e2a3912b73
Marcin Wawrzkow <marcin.wawrzkow@outlook.com>
2019-05-17 00:05:36 +0200

    Adding Perlin class

---
+++ src/GameLogic/Terrain/Perlin/Perlin.cpp
@@ -0,0 +1,21 @@
+/*
+ * Perlin.cpp
+ *
+ *  Created on: May 16, 2019
+ *      Author: mwawrzkow
+ */
+
+#include "Perlin.h"
+
+namespace NoiseAlgorithm {
+
+Perlin::Perlin() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Perlin::~Perlin() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace NoiseAlgorithm */
---
+++ src/GameLogic/Terrain/Perlin/Perlin.h
@@ -0,0 +1,21 @@
+/*
+ * Perlin.h
+ *
+ *  Created on: May 16, 2019
+ *      Author: mwawrzkow
+ */
+
+#ifndef SRC_GAMELOGIC_TERRAIN_PERLIN_PERLIN_H_
+#define SRC_GAMELOGIC_TERRAIN_PERLIN_PERLIN_H_
+
+namespace NoiseAlgorithm {
+
+class Perlin {
+public:
+	Perlin();
+	virtual ~Perlin();
+};
+
+} /* namespace NoiseAlgorithm */
+
+#endif /* SRC_GAMELOGIC_TERRAIN_PERLIN_PERLIN_H_ */

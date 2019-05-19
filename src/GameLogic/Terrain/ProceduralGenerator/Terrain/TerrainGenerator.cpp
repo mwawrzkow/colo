174fbbc9e0dcbd88f94dae94a5333226c5752139
Marcin Wawrzkow <marcin.wawrzkow@outlook.com>
2019-05-19 18:16:19 +0200

    Procedural Generator changes
    	* Delete Perlin Class
    	* Creating Terrain Generator Class
    
    todo list:
    	* create classes for every algorithm

---
+++ src/GameLogic/Terrain/ProceduralGenerator/Terrain/TerrainGenerator.cpp
@@ -0,0 +1,21 @@
+/*
+ * TerrainGenerator.cpp
+ *
+ *  Created on: May 17, 2019
+ *      Author: mwawrzkow
+ */
+
+#include "TerrainGenerator.h"
+
+namespace Generator {
+
+TerrainGenerator::TerrainGenerator() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TerrainGenerator::~TerrainGenerator() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace Generator */
---
+++ src/GameLogic/Terrain/ProceduralGenerator/Terrain/TerrainGenerator.h
@@ -0,0 +1,21 @@
+/*
+ * TerrainGenerator.h
+ *
+ *  Created on: May 17, 2019
+ *      Author: mwawrzkow
+ */
+
+#ifndef SRC_GAMELOGIC_TERRAIN_PROCEDURALGENERATOR_TERRAIN_TERRAINGENERATOR_H_
+#define SRC_GAMELOGIC_TERRAIN_PROCEDURALGENERATOR_TERRAIN_TERRAINGENERATOR_H_
+
+namespace Generator {
+
+class TerrainGenerator {
+public:
+	TerrainGenerator();
+	virtual ~TerrainGenerator();
+};
+
+} /* namespace Generator */
+
+#endif /* SRC_GAMELOGIC_TERRAIN_PROCEDURALGENERATOR_TERRAIN_TERRAINGENERATOR_H_ */

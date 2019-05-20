1184f95318bc1cc9c9765a26fdfb1ab87dfecb71
Marcin Wawrzkow <marcin.wawrzkow@outlook.com>
2019-05-20 18:46:42 +0200

    Working Standard Deviation Class

--- src/GameLogic/Terrain/ProceduralGenerator/Terrain/Scripts/Gauss.cpp
+++ src/GameLogic/Terrain/ProceduralGenerator/Terrain/Scripts/Gauss.cpp
@@ -9,10 +9,58 @@
 
 namespace Script {
 
-Gauss::Gauss(std::vector<float*>) {
+Gauss::Gauss(TerrainUnits::Tile &Tile):array(Tile.getMatrix()),vector(Tile.getVector()) {
 	// TODO Auto-generated constructor stub
+    CalculateSum();
 
 }
+void Gauss::CalculateSum(){
+    float  sum = 0; 
+    for(float *e: array)
+    {
+     sum += *e;   
+    }
+    this->sum = sum;
+    this->avarge = sum /  100;
+    calStandardDeviation();
+    
+}
+void Gauss::calStandardDeviation(){
+    std::vector<float> diferences;
+    for(float *e: array)
+    diferences.push_back(sqrt(pow(avarge-*e,2)));
+    float standardDeviation = 0; 
+    for(float &e: diferences)
+        standardDeviation+= pow(e,2);
+    this->standartDeviation = sqrt(standardDeviation);
+    result();
+}
+void Gauss::result(){
+    for(float*e: array)
+    {
+        float toexp;
+        toexp = 1/(standartDeviation* sqrt(2*M_PI));
+        toexp*= exp((-1**e-avarge)/(2*pow(standartDeviation,2)));
+        gaussresult.push_back(toexp);
+    }
+    
+}
+
+void Gauss::partGaussCalculation(int startx,int starty, int endx, int endy){
+    for(int e = startx; e <= endx;e++){
+        for(int z = starty; z <= endy;z++ )
+        {
+            *array.at(e+z)=gaussresult.at(e+z);
+        }
+    }
+    
+}
+float  Gauss::getStandartDeviation(){
+    return standartDeviation;
+}
+
+
+
 
 Gauss::~Gauss() {
 	// TODO Auto-generated destructor stub
--- src/GameLogic/Terrain/ProceduralGenerator/Terrain/Scripts/Gauss.h
+++ src/GameLogic/Terrain/ProceduralGenerator/Terrain/Scripts/Gauss.h
@@ -9,14 +9,23 @@
 #ifndef SRC_GAMELOGIC_TERRAIN_PROCEDURALGENERATOR_TERRAIN_SCRIPTS_GAUSS_H_
 #define SRC_GAMELOGIC_TERRAIN_PROCEDURALGENERATOR_TERRAIN_SCRIPTS_GAUSS_H_
 #include "../../../Chunk/Tile/Tile.h"
+#include <math.h>
 namespace Script {
 
 class Gauss {
-
-
+    std::vector<float*> array;
+    float standartDeviation;
+    Math::Vector *vector;
+    float sum, avarge;
+    void CalculateSum();
+    void calStandardDeviation();
+    std::vector<float> gaussresult;
+    void result();
 public:
-	Gauss(std::vector<float*>);
+	Gauss(TerrainUnits::Tile&);
 	virtual ~Gauss();
+    void partGaussCalculation(int,int,int,int);
+    float getStandartDeviation();
 };
 
 } /* namespace Script */

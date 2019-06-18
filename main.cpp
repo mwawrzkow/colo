8079f5b7efc3e60ae86a319971a4418541a5210f
Marcin Wawrzkow <marcin.wawrzkow@outlook.com>
2019-06-18 16:42:12 +0200

    New Main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <iomanip>
+#include "src/GameLogic/Map.h"
 
 int main()
 {
 	std::cerr << "Here Magic Begins" << std::endl;
-
+	srand(time(NULL));
+	World::Map map(1000,15);
+	int width = map.getWidth(), height = map.getHeight();
+	for(int x = 0; x < width; ++x){
+		for(int y = 0; y < height; ++y)
+		{
+			std::cerr <<std::setw(15)<<map.getPointValue(x,y) ;
+		}
+		std::cerr << std::endl;
+	}
 
 
 	return 0;

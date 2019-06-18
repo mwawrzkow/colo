0c92cbe18c064fd4223d9f81f9db0a675289ae06
Marcin Wawrzkow <marcin.wawrzkow@outlook.com>
2019-06-18 23:12:29 +0200

    Update Player Class

--- src/GameLogic/Players/Player.cpp
+++ src/GameLogic/Players/Player.cpp
@@ -14,6 +14,44 @@ Player::Player(int iron, int copper, int wood, int gold) :
 	// TODO Auto-generated constructor stub
 
 }
+void Player::playerTick() {
+	citizens = 0;
+	for (Buildings::Home &e : Homes) {
+		citizens += e.getCitizens();
+	}
+	for (Buildings::Mine &e : Mines) {
+		switch (e.getMiningOreType()) {
+		case DeadNature::oreType::Chrome:
+			Chrome += e.gerResources();
+			break;
+		case DeadNature::oreType::Coal:
+			Coal += e.gerResources();
+			break;
+		case DeadNature::oreType::Copper:
+			Copper += e.gerResources();
+			break;
+		case DeadNature::oreType::Diamond:
+			Diamond += e.gerResources();
+			break;
+		case DeadNature::oreType::Ethyrald:
+			Ethyrald+= e.gerResources();
+			break;
+		case DeadNature::oreType::Gold:
+			Gold+= e.gerResources();
+			break;
+		case DeadNature::oreType::Iron:
+			Iron+= e.gerResources();
+			break;
+		case DeadNature::oreType::Magnesium:
+			Magnesium+= e.gerResources();
+			break;
+		case DeadNature::oreType::Uran:
+			Uran+=e.gerResources();
+			break;
+		}
+	}
+
+}
 
 Player::~Player() {
 	// TODO Auto-generated destructor stub
--- src/GameLogic/Players/Player.h
+++ src/GameLogic/Players/Player.h
@@ -7,15 +7,20 @@
 
 #ifndef SRC_GAMELOGIC_PLAYERS_PLAYER_H_
 #define SRC_GAMELOGIC_PLAYERS_PLAYER_H_
-
+#include <vector>
+#include "../Units/Buildings/Home.h"
+#include "../Units/Buildings/Mine.h"
 namespace Player {
 
 class Player {
 	float Iron = { }, Copper = { }, Gold = { }, Diamond = { }, Chrome = { },
 			Ethyrald = { }, Uran = { }, Coal = { }, Magnesium = { }, Wood = {};
-
+	int citizens = {};
+	std::vector<Buildings::Home> Homes;
+	std::vector<Buildings::Mine> Mines;
 public:
 	Player(int iron, int copper,int wood, int gold);
+	void playerTick();
 	virtual ~Player();
 };
 
